a6fba9a00e950a682267f7457318304034631402
chrisguttandin <chrisguttandin@media-codings.com>
2017-06-30 18:40:58 +0200

    test slide navigation

--- test/e2e/home.po.ts
+++ test/e2e/home.po.ts
@@ -8,6 +8,10 @@ export class HomePage {
         return element(by.css('wam-app h1')).getText();
     }
 
+    public getSubHeadline() {
+        return element(by.css('wam-app h2')).getText();
+    }
+
     public navigateTo() {
         return browser.get((IS_SMOKE_TEST) ? 'https://chrisguttandin.github.io/web-audio-meetup-march-2017' : '/');
     }
