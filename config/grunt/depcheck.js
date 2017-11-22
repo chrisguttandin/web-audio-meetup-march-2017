5fd635e4940f1d790213a4d286f7bd82daaed38a
chrisguttandin <chrisguttandin@media-codings.com>
2017-11-22 12:57:54 +0100

    build(config): update depcheck config

--- config/grunt/depcheck.js
+++ config/grunt/depcheck.js
@@ -34,7 +34,8 @@ module.exports = {
                 'tslint',
                 'tslint-config-holy-grail',
                 'typescript',
-                'webpack-bundle-analyzer'
+                'webpack-bundle-analyzer',
+                'webpack-stats-duplicates'
             ]
         },
         src: './'
