5ad6cd1ddf9b1894eee003bab7b4b616cae1dd00
Christoph Guttandin <chrisguttandin@media-codings.com>
2018-11-18 16:27:52 +0100

    style: add missing return types

--- test/e2e/home.po.ts
+++ test/e2e/home.po.ts
@@ -1,21 +1,22 @@
 import { browser, by, element } from 'protractor';
+import { promise } from 'selenium-webdriver'; // tslint:disable-line:no-implicit-dependencies
 
 const IS_SMOKE_TEST = !!process.env.IS_SMOKE_TEST;
 
 export class HomePage {
 
-    public async getHeadline () {
+    public async getHeadline (): Promise<string> {
         const innerHtml = await element(by.css('wam-app h1')).getInnerHtml();
 
         // @todo Chrome does treat a <br> tag as line break but Safari doesn't which is why it is done manually here.
         return innerHtml.replace(/<br>/, '\n');
     }
 
-    public getSubHeadline () {
+    public getSubHeadline (): promise.Promise<string> {
         return element(by.css('wam-app h2')).getText();
     }
 
-    public navigateTo () {
+    public navigateTo (): promise.Promise<any> {
         return browser.get((IS_SMOKE_TEST) ? 'https://chrisguttandin.github.io/web-audio-meetup-march-2017' : '/');
     }
 
