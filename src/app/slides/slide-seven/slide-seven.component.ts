7a52801e7a10cdef1bdbb5c109a908feeea3781c
Christoph Guttandin <chrisguttandin@media-codings.com>
2019-04-09 14:18:09 +0200

    fix: condense header of boilerplate slides

--- src/app/slides/slide-seven/slide-seven.component.ts
+++ src/app/slides/slide-seven/slide-seven.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
     changeDetection: ChangeDetectionStrategy.OnPush,
+    styleUrls: [ './slide-seven.component.css' ],
     templateUrl: './slide-seven.component.html'
 })
 export class SlideSevenComponent { }
--- src/app/slides/slide-six/slide-six.component.ts
+++ src/app/slides/slide-six/slide-six.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
     changeDetection: ChangeDetectionStrategy.OnPush,
+    styleUrls: [ './slide-six.component.css' ],
     templateUrl: './slide-six.component.html'
 })
 export class SlideSixComponent { }
