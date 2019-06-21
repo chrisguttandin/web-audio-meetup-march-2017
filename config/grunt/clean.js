fca8a12eb9261963e111bbcaf0b783d75709475b
Christoph Guttandin <chrisguttandin@media-codings.com>
2019-06-21 21:59:56 +0200

    build: fix clean task

--- config/grunt/clean.js
+++ config/grunt/clean.js
@@ -1,6 +1,6 @@
 module.exports = {
     'runtime': [
-        'build/web-audio-meetup-march-2017/runtime-es*.js'
+        'build/web-audio-meetup-march-2017/runtime*.js'
     ],
     'scripts': [
         'build/web-audio-meetup-march-2017/!(ngsw-worker).js'
