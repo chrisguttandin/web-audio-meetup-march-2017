5255963b1fcf204f3de8b765c70897492d122a4b
Christoph Guttandin <chrisguttandin@media-codings.com>
2019-12-24 23:42:02 +0100

    test: use getAttribute() instead of getInnerHtml()

--- test/e2e/home.po.ts
+++ test/e2e/home.po.ts
@@ -5,10 +5,10 @@ import { promise } from 'selenium-webdriver'; // tslint:disable-line:no-implicit
 export class HomePage {
 
     public async getHeadline (): Promise<string> {
-        const innerHtml = await element(by.css('wam-app h1')).getInnerHtml();
+        const innerHtml = await element(by.css('wam-app h1')).getAttribute('innerHTML');
 
         // @todo Chrome does treat a <br> tag as line break but Safari doesn't which is why it is done manually here.
-        return innerHtml.replace(/<br>/, '\n');
+        return innerHtml.replace(/<br[^>]*>/g, '\n');
     }
 
     public getSubHeadline (): promise.Promise<string> {
