8eecdbc499c63bb8f74abed4e142d5a75cd4375e
Christoph Guttandin <chrisguttandin@media-codings.com>
2020-12-06 21:41:45 +0100

    build(config): update grunt config

--- config/grunt/sh.js
+++ config/grunt/sh.js
@@ -28,21 +28,21 @@ module.exports = (grunt) => {
             cmd: `eslint --config config/eslint/config.json --ext .js ${fix ? '--fix ' : ''}--report-unused-disable-directives *.js config/`
         },
         'lint-src': {
-            cmd: `htmlhint --rules ${convertConfig(documentConfig)} 'src/**/index.html' && htmlhint --rules ${convertConfig(
-                templateConfig
-            )} 'src/app/**/*.component.html' && ng lint web-audio-meetup-march-2017 --type-check`
+            cmd: `htmlhint --rules ${convertConfig(documentConfig)} 'src/**/index.html' && \
+                htmlhint --rules ${convertConfig(templateConfig)} 'src/app/**/*.component.html' && \
+                ng lint web-audio-meetup-march-2017 --type-check`
         },
         'lint-test': {
             cmd: 'ng lint web-audio-meetup-march-2017 --configuration test'
         },
         'monitor': {
-            cmd: 'ng serve --aot'
+            cmd: 'ng serve'
         },
         'performance': {
             cmd: 'karma start config/karma/config-performance.js --single-run'
         },
         'preview': {
-            cmd: 'ng serve --aot --prod'
+            cmd: 'ng serve --prod'
         },
         'smoke': {
             cmd: env.TRAVIS
@@ -53,8 +53,8 @@ module.exports = (grunt) => {
             cmd: 'ng test --watch false'
         },
         'verify': {
-            cmd:
-                "bundle-buddy build/web-audio-meetup-march-2017/*.js.map && grep -r build/**/*.map -e '/environments/environment.ts'; test $? -eq 1"
+            cmd: `bundle-buddy build/web-audio-meetup-march-2017/*.js.map && \
+                grep -r build/**/*.map -e '/environments/environment.ts'; test $? -eq 1`
         }
     };
 };
