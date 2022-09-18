8049700bbb20b3817829a1f0eab18c3078e47603
Christoph Guttandin <chrisguttandin@media-codings.com>
2022-09-19 00:24:18 +0200

    build(config): use Playwright instead of Protractor

--- test/e2e/home.po.ts
+++ test/e2e/home.po.ts
@@ -1,21 +1,17 @@
-import { browser, by, element, promise } from 'protractor';
+import { Locator, Page, Response } from '@playwright/test';
 
-export class HomePage {
-    // eslint-disable-next-line class-methods-use-this
-    public async getHeadline(): Promise<string> {
-        const innerHtml = await element(by.css('wam-app h1')).getAttribute('innerHTML');
+export class Home {
+    constructor(private _page: Page) {}
 
-        // @todo Chrome does treat a <br> tag as line break but Safari doesn't which is why it is done manually here.
-        return innerHtml.replace(/<br[^>]*>/g, '\n');
+    public getHeadline(): Locator {
+        return this._page.locator('wam-app h1');
     }
 
-    // eslint-disable-next-line class-methods-use-this
-    public getSubHeadline(): promise.Promise<string> {
-        return element(by.css('wam-app h2')).getText();
+    public getSubHeadline(): Locator {
+        return this._page.locator('wam-app h2:first-child');
     }
 
-    // eslint-disable-next-line class-methods-use-this
-    public navigateTo(): promise.Promise<unknown> {
-        return browser.get(browser.baseUrl);
+    public navigateTo(): Promise<null | Response> {
+        return this._page.goto('./');
     }
 }
