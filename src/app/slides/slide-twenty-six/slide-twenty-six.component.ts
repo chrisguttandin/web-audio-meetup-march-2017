8186c490018510b7f769dc029d77175fb5ea3c19
Christoph Guttandin <chrisguttandin@media-codings.com>
2023-04-19 03:07:29 +0200

    style: remove empty css file

--- src/app/slides/slide-twenty-six/slide-twenty-six.component.ts
+++ src/app/slides/slide-twenty-six/slide-twenty-six.component.ts
@@ -2,7 +2,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
     changeDetection: ChangeDetectionStrategy.OnPush,
-    styleUrls: ['./slide-twenty-six.component.css'],
     templateUrl: './slide-twenty-six.component.html'
 })
 export class SlideTwentySixComponent {}
